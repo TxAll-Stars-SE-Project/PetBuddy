054e1b6ad8f8ed3d2d5ef3cce3c13cc464d505c5
tanravikorn <tanravikorn@gmail.com>
2026-09-04 19:55:12 +0700

    fix(syntax) : Normalize systax for prisma.ts

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -5,22 +5,8 @@ import pg from 'pg';
 
 dotenv.config();
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+export const prisma = new PrismaClient({ adapter });
 
 export default prisma;
